834744fea446d5cc43ee86dcf9739d466fae1958
ErikRoepman <62939461+ErikRoepman@users.noreply.github.com>
2021-04-22 17:08:04 +0200

    gitignore, render image

--- src/components/BackgroundImage.tsx
+++ src/components/BackgroundImage.tsx
@@ -13,12 +13,10 @@ export class BackgroundImage extends Component<ImageProps> {
                 return <div className="image">Loading...</div>;
             }
             case ValueStatus.Available:{
-                console.log(image)
-                if (image.value.uri)
-                    return <img className="image" src={image.value.uri}/>; 
-                else
-                    return <div className="image">Please provide a background image</div>;
+                return <img className="image" src={image.value.uri}/>; 
             }
+            default:
+               return <div className="image"></div>; 
         }
     }
 }
