607278d588f9c84e71b714a44062c112af97e5ba
MatteoTimmermans <75371563+MatteoTimmermans@users.noreply.github.com>
2021-04-26 13:42:34 +0200

    Print message on click

--- src/InteractiveImage.tsx
+++ src/InteractiveImage.tsx
@@ -1,15 +1,26 @@
-import { Component, ReactNode, createElement } from "react";
+import { Component, ReactNode, createElement, MouseEvent} from "react";
 
 import { InteractiveImageContainerProps } from "../typings/InteractiveImageProps";
 
 import "./ui/InteractiveImage.css";
 
 export default class InteractiveImage extends Component<InteractiveImageContainerProps> {
+    let a = document.createElement ("X");
+
+
+    //Function prints coordinates of click to log
+    Coordinates(event: MouseEvent){
+    let x1 = event.clientX
+    let y1 = event.clientY
+    console.log("I clicked on " + x1 + ' ' + y1);
+    
+}
+    //Show map image
     render(): ReactNode {
         const {bgImage} = this.props
         if(bgImage?.value !== undefined) {
             console.log(bgImage);
-            return <img className= "bgImage" src={bgImage.value.uri}/>
+            return <img className= "bgImage" onClick = {this.Coordinates} src={bgImage.value.uri }/>
         }
 
        else return <div>please upload an image</div>;
